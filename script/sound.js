358f3adb707c91bf4ef34c8bc2bedab3df4eaf17
TillmanW <Tillmanwendorff9@gmail.com>
2026-03-11 17:24:03 +0000

    Wed, Mar 11, 2026, 10:24 AM -07:00

---
+++ script/sound.js
@@ -0,0 +1,17 @@
+buttons = document.querySelector('.buttons')
+
+sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+
+for (let i = 0; i < 6; i = i + 1) {
+    btn = document.createElement('button')
+    btn.innerText = sounds[i]
+    btn.classList.add('btn')
+    btn.addEventListener('click', () => {
+        document.querySelector(`.${sounds[i]}`).play()
+    })
+    buttons.appendChild(btn)
+}
+
+
+

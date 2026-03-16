cef27f2c9159c7d842535672b7b9cf9e3ef607e2
TillmanW <Tillmanwendorff9@gmail.com>
2026-03-16 17:04:00 +0000

    Mon, Mar 16, 2026, 10:04 AM -07:00

--- script/nav.js
+++ script/nav.js
@@ -1,5 +1,7 @@
 nav = document.querySelector('.nav')
 
 window.addEventListener('scroll', () => {
-    console.log(scrollY)
+    if (scrollY == 420) {
+        nav.classList.add('active')
+    }
 })

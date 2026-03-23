251c39403fdb5ec4eb93a0a6ef56d358beb521db
TillmanW <Tillmanwendorff9@gmail.com>
2026-03-23 16:43:54 +0000

    Mon, Mar 23, 2026, 9:43 AM -07:00

---
+++ script/insect.js
@@ -0,0 +1,52 @@
+screens = document.querySelectorAll('.screen')
+choose-insect_btns= document.querySelectorAll('.choose-insect-btn')
+start_btn = document.getElementById('start-btn')
+
+
+start_btn.addEventListener('click', () => {
+    screens [0].classList.add('up')
+})
+
+for (let i = 0; i < choose_insect_btns.length; i = i + 1)
+{
+    choose_insect_btns[i].addEventListener('click', () => {
+        //screens[1].classList.add('up')
+        img = choose_insect_btns[i].querySelector('img')
+        src = img.getAttribute('src')
+        alt = img.getAttribute('alt')
+        startGame()
+
+    })
+}
+
+
+function startGame() {
+    setTimeout(createInsect, 1000)
+}
+
+function createInsect() {
+    insect = document.createElement('div')
+    insect.classList.add('insect')
+    const {x,y} = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHtml = `<img src="${src}" alt="${alt}" style="transform: rotate(${Math.random()*360}deg)>`
+    game_container.appendChild(insect)
+}
+
+function getRandomLocation() {
+    width = window.innerWidth
+    height = window.innerHeight
+    x = Math.random()*(width-200) + 100
+    y = Math.random()*(height - 200) + 100
+}
+
+
+
+function catchInsect() {
+    //increaseScore()
+    this.classList.add('caught')
+    setTimeout( () => this.remove(), 2000)
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
+}

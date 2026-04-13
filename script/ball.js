5b780223015bd48e6f6a6a12cfbfe4e4247f56e7
TillmanW <Tillmanwendorff9@gmail.com>
2026-04-13 16:10:59 +0000

    Mon, Apr 13, 2026, 9:10 AM -07:00

--- script/ball.js
+++ script/ball.js
@@ -3,14 +3,18 @@ const ballRadius = 30
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
 
+
+function moveBall(){
+    ball.style.left = ?
+}
 createBall()
 function createBall(){
     document.body.appendChild(ball)
     ball.style.height = `${2 * ballRadius}px`
-    ball.style.width = "100px"
+    ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
     ball.style.backgroundColor = "green"
     ball.style.position = "absolute"
-    ball.style.top = "30px"
-    ball.style.left = "30px"
+    ball.style.top = `${windowHeight/2 - ballRadius}px`
+    ball.style.left = `${windowWidth/2 - ballRadius}px`
 }
